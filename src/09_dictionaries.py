2d14187bb7dc5bda507095f86e97df434930d6ea
bejolo018 <bejolo018@gmail.com>
2019-09-04 10:36:38 -0500

    got them all except 14

--- src/09_dictionaries.py
+++ src/09_dictionaries.py
@@ -35,10 +35,16 @@ waypoints = [
 
 # Add a new waypoint to the list
 # YOUR CODE HERE
+waypoints.append({ "lat": 99, "lon": 33, "name": "another nother place"})
 
 # Modify the dictionary with name "a place" such that its longitude
 # value is -130 and change its name to "not a real place"
 # YOUR CODE HERE
-
+for d in waypoints:
+     if "a place" in d.values():
+         d["lon"] = -130
+         d["name"] = "not a real place"
 # Write a loop that prints out all the field values for all the waypoints
 # YOUR CODE HERE
+for d in waypoints:
+    print(d.values())
