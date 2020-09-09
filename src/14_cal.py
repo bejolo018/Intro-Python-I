ef9e80bb2991ec857e57448df6b9257baa8b1ee1
Benjamin Lopez <47269255+bejolo018@users.noreply.github.com>
2020-09-08 19:36:33 -0500

    completed second half of project

--- src/14_cal.py
+++ src/14_cal.py
@@ -4,7 +4,7 @@ render a calendar to your terminal.
 https://docs.python.org/3.6/library/calendar.html
 
 Write a program that accepts user input of the form
-  `14_cal.py month [year]`
+  `14_cal.py [month] [year]`
 and does the following:
  - If the user doesn't specify any input, your program should
    print the calendar for the current month. The 'datetime'
@@ -17,8 +17,40 @@ and does the following:
  - Otherwise, print a usage statement to the terminal indicating
    the format that your program expects arguments to be given.
    Then exit the program.
+
+Note: the user should provide argument input (in the initial call to run the file) and not 
+prompted input. Also, the brackets around year are to denote that the argument is
+optional, as this is a common convention in documentation.
+
+This would mean that from the command line you would call `python3 14_cal.py 4 2015` to 
+print out a calendar for April in 2015, but if you omit either the year or both values, 
+it should use today’s date to get the month and year.
 """
 
 import sys
 import calendar
 from datetime import datetime
+
+l = len(sys.argv)
+
+if l == 1:
+    # User didn't specify any input
+    month = datetime.now().month
+    year = datetime.now().year
+elif l == 2:
+    # User didn't specify year
+    month = int(sys.argv[1])
+    year = datetime.now().year
+elif l == 3:
+    # User specified both month and year
+    month = int(sys.argv[1])
+    year = int(sys.argv[2])
+else:
+    # User provided faulty input
+    print("usage: calendar.py [month] [year]")
+    sys.exit(1)
+
+cal = calendar.TextCalendar()
+
+cal.prmonth(year, month)
+
